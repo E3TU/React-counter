2d21af3b6f1d5c3e443b9a86498f86b8a15bd747
Eetu <99178022+E3TU@users.noreply.github.com>
2024-01-15 15:53:29 +0200

    Logic for the counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  let increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  let decreaseCount = () => {
+    setCount(count - 1);
+  };
+
+  let resetCount = () => {
+    setCount(0);
+  }
+
   return (
     <div className="App">
       <div className="container">
       <h1 className="heading">Counter App</h1>
-        <h1 className="count">10</h1>
+        <h1 className="count">{count}</h1>
         <div className="button-container">
-          <button id="decrement">-</button>
-          <button>Reset</button>
-          <button id="increment">+</button>
+          <button id="decrement" onClick={decreaseCount}>-</button>
+          <button onClick={resetCount}>Reset</button>
+          <button onClick={increaseCount} id="increment">+</button>
         </div>
       </div>
     </div>
